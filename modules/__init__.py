f392e35595b2708f3080115290d0d62b75909f6a
sienlonglim <limsienlong@gmail.com>
2024-08-04 15:48:02 +0800

    Complete job link scrapping module

---
+++ modules/__init__.py
@@ -0,0 +1,1 @@
+from .job_finder import JobFinder
--- modules/job_finder.py
+++ modules/job_finder.py
@@ -1,11 +1,9 @@
-import os
-import requests, re, time, logging
+import sys
+import re
+import time
+import logging
 from typing import Optional
-from datetime import datetime
-from functools import wraps
 
-
-import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.common.by import By
@@ -14,28 +12,30 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import FirefoxOptions
 from bs4 import BeautifulSoup
 
-from .config import DEBUG, HEADER
+from .config import JobFinderConfig
 
 
-log = logging.basicConfig()
+logging.basicConfig(level=logging.INFO, stream=sys.stdout, format="%(message)s")
+log = logging.getLogger()
 
 
 class JobFinder:
     def __init__(
         self,
-        header: Optional[dict] = HEADER,
-        debug: Optional[bool] = False
+        header: Optional[dict] = JobFinderConfig.HEADER,
+        debug: Optional[bool] = JobFinderConfig.DEBUG
     ):
         self.header = header
         self.debug = debug
         self.driver = None
+        self.job_links_dict = None
 
     def _create_firefox_driver(
         self,
         mode: str = "--headless"
     ) -> None:
         firefox_options = FirefoxOptions()
-        if not DEBUG:
+        if not self.debug:
             firefox_options.add_argument(mode)
         self.driver = webdriver.Firefox(options=firefox_options)
         self.driver.implicitly_wait(30)
@@ -46,17 +46,17 @@ class JobFinder:
         max_retries: int = 3
     ) -> WebDriver:
         self._create_firefox_driver()
-        if (self.driver is not None) and (retry_count < max_retries):
+        if (self.driver is None) or (retry_count >= max_retries):
+            return None
+        else:
             self.driver.get(f"https://www.linkedin.com/jobs/search/?distance=25&geoId=102454443&keywords={self.current_title_for_url}&location=Singapore&start=0")
+            time.sleep(5)
             html_source = self.driver.page_source
             soup = BeautifulSoup(html_source, 'html.parser')
-            if soup.content is not None:
-                if "Sign Up | LinkedIn" in soup.find('title').text:
-                    log.debug("\tRan into AuthWall, trying again in 5 secs...")
-                else:
-                    return self.driver
+            if "Sign Up | LinkedIn" in soup.find('title').text:
+                log.info("\tRan into AuthWall, trying again in 5 secs...")
             else:
-                log.debug("\tEmpty soup, trying again in 5 secs...")
+                return self.driver
             self.driver.quit()
             time.sleep(5)
             return self._get_past_authentication_wall(retry_count+1)
@@ -81,42 +81,55 @@ class JobFinder:
             except Exception as e:
                 print(e)
 
-    def _get_job_url_soup(
+    def _get_job_urls_soup(
         self,
-        keyword: str,
         pages: int,
     ) -> BeautifulSoup:
-        self.current_title_for_url = re.sub(' ', '%20', keyword.lower())
-        self.current_keyword = re.sub(' ', '-', keyword.lower())
-        log.info(f'Searching for {keyword}')
-        self._get_past_authentication_wall(max_retries=3)
-        self._scroll_pages(pages=pages)
+        if self._get_past_authentication_wall(max_retries=3) is None:
+            raise ValueError("Error getting soup from main page!")
+        else:
+            self._scroll_pages(pages=pages)
+            log.info('Completed page scrolls, downloading page_source')
+            html_source = self.driver.page_source
+            soup = BeautifulSoup(html_source, 'html.parser')
+            self.driver.quit()
+            return soup
 
-        log.info('Getting page_source')
-        html_source = self.driver.page_source
-        soup = BeautifulSoup(html_source, 'html.parser')
-        self.driver.quit()
-        return soup
-    
-    def get_job_urls_from_soup(
+    def _get_job_urls_from_soup(
         self,
         soup: BeautifulSoup,
     ) -> dict:
-        data_dict = {}
+        job_links_dict = {}
+
         def custom_selector(tag):
-            return tag.name == "a" and tag.has_attr("href") and self.keyword in tag.get('href')
+            return tag.name == "a" and tag.has_attr("href") and self.current_keyword in tag.get('href')
         tags = soup.find_all(custom_selector)
 
         # Get the link and jobid for each listed job
         for tag in tags:
             link = tag.get('href')
-            link = link.split('?')[0] # Tidy up the link to remove the trackingid
-            if 'login' not in link: # To handle a login link that shows at the end
-                data_dict.setdefault(link, 0)
-                data_dict[link] += 1
-        log.info(f'Unique links: {len(data_dict)}')
-        if self.debug:
-            return data_dict, soup
-        else:
-            return data_dict
-        
+            link = link.split('?')[0]  # Tidy up the link to remove the trackingid
+            if 'login' not in link:  # To handle a login link that shows at the end
+                job_links_dict.setdefault(link, 0)
+                job_links_dict[link] += 1
+        log.info(f'Unique links: {len(job_links_dict)}')
+        return {
+            "job_links": job_links_dict,
+            "soup": soup
+            }
+
+    def retrieve_linkedin_jobs_by_keywords(
+        self,
+        keyword: str,
+        pages: int,
+    ) -> dict:
+        self.current_title_for_url = re.sub(' ', '%20', keyword.lower())
+        self.current_keyword = re.sub(' ', '-', keyword.lower())
+        log.info(f'Searching LinkedIn for {keyword}')
+
+        soup = self._get_job_urls_soup(
+            pages=pages
+        )
+        self.job_links_dict = self._get_job_urls_from_soup(
+            soup=soup
+        )
