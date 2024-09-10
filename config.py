c0617fe6386ee650766b9b4bc31be88e3ddaabe7
sienlonglim <limsienlong@gmail.com>
2024-09-10 11:52:43 +0800

    Update config to read email list correctly

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 import os
-import json
 
 
 @dataclass
@@ -11,7 +10,7 @@ class MainConfig:
         # 'data engineer': 10
     }
     email_subject = "LinkedIn JobFinder Summary"
-    email_recipients = json.loads(os.environ['EMAIL_RECIPIENTS'])
+    email_recipients = [email.strip() for email in os.environ['EMAIL_RECIPIENTS'].split(",") if email != ""]
     email_body = ""
     bucket_name = os.environ["AMAZON_S3_BUCKET_NAME"]
     bucket_subfolder = os.environ["AMAZON_S3_BUCKET_SUBFOLDER"]
